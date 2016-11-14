8ac7b38ea8fbfe7fc21407f5e8db8a639033df9f
CSquared-cscw <csquared.cscw@gmail.com>
2016-11-14 16:11:58 +0000

    Site

---
+++ About/About.js
@@ -0,0 +1,12 @@
+function openCharles(){
+    document.getElementById("CharlesPage").style.display="block";
+}
+function openCharlie(){
+    document.getElementById("CharliePage").style.display="block";
+}
+function closeCharles(){
+    document.getElementById("CharlesPage").style.display="none";
+}
+function closeCharlie(){
+    document.getElementById("CharliePage").style.display="none";
+}
---
+++ Home/HomePage.js
@@ -0,0 +1,24 @@
+function Click1(){
+    document.getElementById("slide1").style.display="block";
+    document.getElementById("slide2").style.display="none";
+    document.getElementById("slide3").style.display="none";
+    document.getElementById("dot1").style.backgroundColor="#9647B7";
+    document.getElementById("dot2").style.backgroundColor="rgba(0, 0, 0, 0.6)";
+    document.getElementById("dot3").style.backgroundColor="rgba(0, 0, 0, 0.6)";
+}
+function Click2(){
+    document.getElementById("slide1").style.display="none";
+    document.getElementById("slide2").style.display="block";
+    document.getElementById("slide3").style.display="none";
+    document.getElementById("dot1").style.backgroundColor="rgba(0, 0, 0, 0.6)";
+    document.getElementById("dot2").style.backgroundColor="#9647B7";
+    document.getElementById("dot3").style.backgroundColor="rgba(0, 0, 0, 0.6)";
+}
+function Click3(){
+    document.getElementById("slide1").style.display="none";
+    document.getElementById("slide2").style.display="none";
+    document.getElementById("slide3").style.display="block";
+    document.getElementById("dot1").style.backgroundColor="rgba(0, 0, 0, 0.6)";
+    document.getElementById("dot2").style.backgroundColor="rgba(0, 0, 0, 0.6)";
+    document.getElementById("dot3").style.backgroundColor="#9647B7";
+}
